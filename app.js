ee7415e92296da8988f0b3d5dbcaaef79b3718b3
Thufeil Shobrun Jamil <112791948+thufel@users.noreply.github.com>
2022-11-19 19:03:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const indexRouter = require('../food-journal/routes/routes')
 
 const app = express()
-const port = 3000
+const port = process.env.port || 3000
 
 app.use(express.json())
 app.use('/', indexRouter)
